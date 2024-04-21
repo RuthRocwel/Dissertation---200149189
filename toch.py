736ff0dc12897f73bf169d869815440e80a3afb6
RuthRocwel <114992405+RuthRocwel@users.noreply.github.com>
2024-04-21 17:32:18 +0100

    toch.py

--- toch.py
+++ toch.py
@@ -1,8 +1,6 @@
 from xml.parsers.expat import model
 import torch
 
-# Assuming 'model' is your trained PyTorch model
-# Train your model and then save it
 torch.save(model.state_dict(), 'traffic_prediction_model.pth')
 
 print("Model saved successfully.")
